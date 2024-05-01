c3b305a3a4e0f8ad0b689e2919bdf4de215a3d0b
Ajin Varughese <92434245+Ajinvarughese@users.noreply.github.com>
2024-05-01 15:25:05 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 const hamBurger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
 
-
 hamBurger.addEventListener("click", () => {
     hamBurger.classList.toggle("active");
     navMenu.classList.toggle("active");
